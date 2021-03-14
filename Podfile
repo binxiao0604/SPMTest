cc6a925af976e628c758fa2907b5de81aa5714cd
HotpotCat <binxiao0604@163.com>
2021-03-15 05:57:32 +0800

    init

---
+++ Podfile
@@ -0,0 +1,8 @@
+# Uncomment the next line to define a global platform for your project
+platform :ios, '10.0'
+
+target 'SPM' do
+#  use_frameworks!
+  pod 'HandyJSON','5.0.2'
+  pod 'RxSwift','5.1.2'
+end
---
+++ SPM/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  SPM
+//
+//  Created by ZP on 2021/3/15.
+//
+
+import UIKit
+import RxSwift
+import HandyJSON
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
