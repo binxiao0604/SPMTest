fc2ee8ff8855a21704f67e22e69178cb083652cf
zhangpeng <zhangpeng4@haier.com>
2021-03-16 17:24:36 +0800

    引入Swift Package for OC
    Change-Id: Ifa71bcecc4d6c555c5745bba5c0994744bcade73

--- Podfile
+++ Podfile
@@ -4,5 +4,5 @@ platform :ios, '10.0'
 target 'SPM' do
 #  use_frameworks!
   pod 'HandyJSON','5.0.2'
-  pod 'RxSwift','5.1.2'
+#  pod 'RxSwift','5.1.2'
 end
--- SPM/ViewController.swift
+++ SPM/ViewController.swift
@@ -14,7 +14,22 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        //调用三方 Swift Package
+        let rxError = RxError.argumentOutOfRange
+        print("RxSwift \(rxError)")
+        
+        //调用自定义Swift Package
+        print("SwiftPackageByXcode")
         SwiftPackageByXcode().test()
+        
+        //调用三方 Swift Package OC
+        let manager = AFHTTPSessionManager(baseURL: URL(string: "https:www.baidu.com"))
+        print("AFNetworking \(manager)")
+        
+        //调用自定义Swift Package OC/C/C++
+        testC()
+        SPMTestOC().testOC()
+        SPMTestMixed().testCplus()
     }
 }
 
